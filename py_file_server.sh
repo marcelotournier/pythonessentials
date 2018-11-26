a756e6367415d9d1703d0ba8ff230fbaf2663d21
marcelotournier <dr.tournier@gmail.com>
2018-11-25 19:06:56 -0800

    Add files via upload

---
+++ py_file_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install python file server
+sudo apt-get install -y python-setuptools python-dev build-essential &&
+sudo easy_install pip &&
+sudo pip install HTTPServer &&
+echo "Go to the directory with files you want to serve, and run: "sudo python -m SimpleHTTPServer 8080" 
+
