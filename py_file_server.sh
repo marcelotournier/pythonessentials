feabd28e7578eedf7cf308f10472f78b4a9a6ad8
marcelotournier <dr.tournier@gmail.com>
2018-11-25 19:14:21 -0800

    Update py_file_server.sh

--- py_file_server.sh
+++ py_file_server.sh
@@ -3,5 +3,10 @@
 sudo apt-get install -y python-setuptools python-dev build-essential &&
 sudo easy_install pip &&
 sudo pip install HTTPServer &&
-echo 'Go to the directory with files you want to serve, and run:'
-echo '"sudo python -m SimpleHTTPServer 8080"'
+echo
+echo
+echo
+echo DONE!
+echo
+echo Now, go to the directory with files you want to serve, and run:
+echo sudo python -m SimpleHTTPServer 8080
